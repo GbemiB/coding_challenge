f700b2c764cdcddba4a03eb9dcc3d07f41566831
Taiwo  Gbemisola Bello <tbello@ecobank.com>
2022-03-06 17:45:31 +0100

    Repeat string

--- src/main/java/com/codingchallenge/codewar/withoutjunit/RepeatString.java
+++ src/main/java/com/codingchallenge/codewar/withoutjunit/RepeatString.java
@@ -1,12 +1,15 @@
 package com.codingchallenge.codewar.withoutjunit;
 
 public class RepeatString {
-    public static String repeat(String str, int n) {
-        // n is the number to times you want the string to be repeated
-        String repeat = str.repeat(n);
-        return repeat;
+    public static String smash(String... words) {
+        // String.join() method takes in delimiter i.e the character linking the arrays of string
+        // in the sentence to be created. and the second thing it takes is the array of string
+        String sentence = String.join(" " , words);
+//        String sentence = String.join("_" , words);
+//        String sentence = String.join("+" , words);
+        return sentence;
     }
     public static void main (String args[]) {
-        System.out.println(repeat("GLEAMS", 10));
+        System.out.println(smash("MY", "NAME", "IS", "GBEMISOLA", "BELLO"));
     }
 }
